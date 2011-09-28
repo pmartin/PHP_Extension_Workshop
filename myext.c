040d9fec60d3084a879d5022d10f368495b3ffbb
Patrick Allaert <pa@ez.no>
2011-09-28 08:52:41 +0200

    Added: native class Int

--- myext.c
+++ myext.c
@@ -14,7 +14,7 @@ zend_module_entry myext_module_entry = {
     STANDARD_MODULE_HEADER,
     "myext",
     NULL, /* Function entries */
-    NULL, /* Module init */
+    PHP_MINIT(myext), /* Module init */
     NULL, /* Module shutdown */
     NULL, /* Request init */
     NULL, /* Request shutdown */
@@ -27,3 +27,102 @@ zend_module_entry myext_module_entry = {
 #ifdef COMPILE_DL_MYEXT
 ZEND_GET_MODULE(myext)
 #endif
+
+zend_class_entry *ce_Int;
+
+static zend_object_handlers int_object_handlers;
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_int___construct, 0, 0, 1)
+    ZEND_ARG_INFO(0, value)
+ZEND_END_ARG_INFO()
+
+static zend_function_entry int_class_functions[] = {
+    PHP_ME( Int, __construct, arginfo_int___construct, ZEND_ACC_PUBLIC|ZEND_ACC_CTOR )
+};
+
+static void int_object_free(void *_object TSRMLS_DC)
+{
+    int_object *object = (int_object*)_object;
+
+    zend_hash_destroy(object->zo.properties);
+    FREE_HASHTABLE(object->zo.properties);
+
+    zval_ptr_dtor(&object->value);
+
+    efree(object);
+}
+
+static zend_object_value int_object_new(zend_class_entry *class_type TSRMLS_DC)
+{
+    zend_object_value retval;
+    int_object *intern;
+    zval *tmp;
+
+    intern = emalloc(sizeof(int_object));
+    memset(intern, 0, sizeof(int_object));
+
+    zend_object_std_init(&intern->zo, class_type TSRMLS_CC);
+	// As of PHP 5.4, next line could be written:
+	// object_properties_init(&intern->zo, class_type);
+    zend_hash_copy(intern->zo.properties, &class_type->default_properties, (copy_ctor_func_t) zval_add_ref, (void *) &tmp, sizeof(zval *));
+
+    retval.handle = zend_objects_store_put(
+        intern,
+        (zend_objects_store_dtor_t) zend_objects_destroy_object,
+        (zend_objects_free_object_storage_t)int_object_free,
+        NULL TSRMLS_CC
+    );
+
+    retval.handlers = &int_object_handlers;
+
+    ALLOC_INIT_ZVAL(intern->value);
+
+    return retval;
+}
+
+static int int_object_cast(zval *readobj, zval *writeobj, int type TSRMLS_DC)
+{
+    int_object *object = (int_object*)zend_object_store_get_object(readobj TSRMLS_CC);
+
+    switch (type)
+    {
+        case IS_STRING:
+        case IS_DOUBLE:
+        case IS_LONG:
+            ZVAL_ZVAL(writeobj, object->value, 1, 0);
+            convert_to_explicit_type(writeobj, type);
+            return SUCCESS;
+    }
+
+    return FAILURE;
+}
+
+PHP_MINIT_FUNCTION(myext)
+{
+    zend_class_entry ce;
+
+    memcpy(&int_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+    int_object_handlers.cast_object = int_object_cast;
+
+    INIT_CLASS_ENTRY(ce, "Int", int_class_functions);
+    ce.create_object = int_object_new;
+    ce_Int = zend_register_internal_class(&ce TSRMLS_CC);
+}
+
+PHP_METHOD( Int, __construct )
+{
+    zval *value;
+    int_object *object;
+    zend_error_handling error_handling;
+
+    zend_replace_error_handling(EH_THROW, NULL, &error_handling TSRMLS_CC);
+
+    if ( zend_parse_parameters( ZEND_NUM_ARGS() TSRMLS_CC, "z", &value ) == SUCCESS )
+    {
+        object = (int_object*)zend_object_store_get_object(getThis() TSRMLS_CC);
+        ZVAL_ZVAL(object->value, value, 1, 0);
+        convert_to_long(object->value);
+    }
+
+    zend_restore_error_handling(&error_handling TSRMLS_CC);
+}
---
+++ myext.php
@@ -0,0 +1,5 @@
+<?php
+$i = new Int( 42 );
+
+echo "Adding an Int class and a regular int: ", $i + 10, "\n";
+echo "Using an Int class as a regular string: ", "value = $i", "\n";
